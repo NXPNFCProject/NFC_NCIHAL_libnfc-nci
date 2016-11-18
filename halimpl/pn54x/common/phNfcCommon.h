4cccac078a23b3a92c22ecbdd5aee8f401535f94
nxpandroid <srikrishna.pramoda@nxp.com>
2016-11-18 21:22:21 +0530

    NFC_NCIHALx_ARFC.7.1.0_N_OpnSrc

--- halimpl/pn54x/common/phNfcCommon.h
+++ halimpl/pn54x/common/phNfcCommon.h
@@ -77,8 +77,8 @@
 #define COMPILATION_MW "PN547C2"
 #endif
 /* HAL Version number (Updated as per release) */
-#define NXP_MW_VERSION_MAJ  (0U)
-#define NXP_MW_VERSION_MIN  (0x0D)
+#define NXP_MW_VERSION_MAJ  (1U)
+#define NXP_MW_VERSION_MIN  (0U)
 
 #define GET_EEPROM_DATA (1U)
 #define SET_EEPROM_DATA (2U)
