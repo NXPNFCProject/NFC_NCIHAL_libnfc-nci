5f0b3145dde57dc55feec1a436b17142a4a0c4b9
nxpandroid <srikrishna.pramoda@nxp.com>
2017-09-15 16:32:42 +0530

    MW version updated to 8.1.0

--- src/nfc/include/nfc_api.h
+++ src/nfc/include/nfc_api.h
@@ -115,11 +115,11 @@
 #define NXP_EN_PN553_MR1 0
 #define NXP_EN_PN81A     0
 #define NXP_EN_PN553_MR2 0
-#define NXP_EN_PN557     1
-#define NXP_EN_PN81T     1
+#define NXP_EN_PN557     0
+#define NXP_EN_PN81T     0
 #define NXP_ANDROID_VER (8U)        /* NXP android version */
-#define NFC_NXP_MW_VERSION_MAJ (0x00) /* MW Major Version */
-#define NFC_NXP_MW_VERSION_MIN (0x18) /* MW Minor Version */
+#define NFC_NXP_MW_VERSION_MAJ (0x01) /* MW Major Version */
+#define NFC_NXP_MW_VERSION_MIN (0x00) /* MW Minor Version */
 #endif
 /* 0xE0 ~0xFF are proprietary status codes */
 /* Command started successfully                     */
