9af83cc5ad890962372f581bb610b06d9e2a8076
nxf24591 <nanjesh.s_1@nxp.com>
2017-10-03 16:10:48 +0530

    MW version updated to 8.2.0

--- src/nfc/include/nfc_api.h
+++ src/nfc/include/nfc_api.h
@@ -118,8 +118,8 @@
 #define NXP_EN_PN557     1
 #define NXP_EN_PN81T     1
 #define NXP_ANDROID_VER (8U)        /* NXP android version */
-#define NFC_NXP_MW_VERSION_MAJ (0x01) /* MW Major Version */
-#define NFC_NXP_MW_VERSION_MIN (0x04) /* MW Minor Version */
+#define NFC_NXP_MW_VERSION_MAJ (0x02) /* MW Major Version */
+#define NFC_NXP_MW_VERSION_MIN (0x00) /* MW Minor Version */
 #endif
 /* 0xE0 ~0xFF are proprietary status codes */
 /* Command started successfully                     */
