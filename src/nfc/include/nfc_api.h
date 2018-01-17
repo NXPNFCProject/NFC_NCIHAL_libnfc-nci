79d7fb2019004e92c34467932da4c4c6f90e94a9
nxf35421 <sangeetha.agarwal@nxp.com>
2018-01-17 17:43:56 +0530

    MW version updated to 8.3.2

--- src/nfc/include/nfc_api.h
+++ src/nfc/include/nfc_api.h
@@ -106,12 +106,12 @@
 // DTA API for MW Version need to change according to release
 #define NXP_EN_PN547C2 0
 #define NXP_EN_PN65T 0
-#define NXP_EN_PN548C2 1
-#define NXP_EN_PN66T 1
-#define NXP_EN_PN551 1
-#define NXP_EN_PN67T 1
-#define NXP_EN_PN553 0
-#define NXP_EN_PN80T 0
+#define NXP_EN_PN548C2 0
+#define NXP_EN_PN66T 0
+#define NXP_EN_PN551 0
+#define NXP_EN_PN67T 0
+#define NXP_EN_PN553 1
+#define NXP_EN_PN80T 1
 #define NXP_EN_PN553_MR1 0
 #define NXP_EN_PN81A     0
 #define NXP_EN_PN553_MR2 0
@@ -119,7 +119,7 @@
 #define NXP_EN_PN81T     0
 #define NXP_ANDROID_VER (8U)        /* NXP android version */
 #define NFC_NXP_MW_VERSION_MAJ (0x03) /* MW Major Version */
-#define NFC_NXP_MW_VERSION_MIN (0x01) /* MW Minor Version */
+#define NFC_NXP_MW_VERSION_MIN (0x02) /* MW Minor Version */
 #endif
 /* 0xE0 ~0xFF are proprietary status codes */
 /* Command started successfully                     */
