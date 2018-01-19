93a925de7d3d742d8d72ba4dd3502bca1d6083a1
Pratap Reddy <vakapratapa.reddy@nxp.com>
2018-01-19 17:54:57 +0530

    Fixed memory leak of 260 bytes

--- halimpl/pn54x/hal/phNxpNciHal.c
+++ halimpl/pn54x/hal/phNxpNciHal.c
@@ -1488,7 +1488,6 @@ int phNxpNciHal_core_initialized(uint8_t* p_core_init_rsp_params) {
         NXP_NCI_HAL_CORE_INIT_RECOVER(retry_core_init_cnt, retry_core_init);
     }
 
-  if (config_success == false) return NFCSTATUS_FAILED;
 #ifdef PN547C2_CLOCK_SETTING
 #if (NFC_NXP_HFO_SETTINGS == TRUE)
     NXPLOG_NCIHAL_D("Applying Default Clock setting and DPLL register at power on");
@@ -2161,7 +2160,10 @@ int phNxpNciHal_core_initialized(uint8_t* p_core_init_rsp_params) {
           updateNxpConfigTimestamp();
       }
   #endif*/
-  return NFCSTATUS_SUCCESS;
+  if (config_success == false)
+    return NFCSTATUS_FAILED;
+  else
+    return NFCSTATUS_SUCCESS;
 }
 /******************************************************************************
  * Function         phNxpNciHal_check_eSE_Session_Identity
