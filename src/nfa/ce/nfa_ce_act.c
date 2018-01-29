f7cb81aab6843aa0d6b3caea56d1dad30c4694e5
Bhupendra Pawar <bhupendra.pawar@nxp.com>
2018-01-29 17:15:45 +0530

    Fix for System code and ID is not registered when Application is lunched.
    t3t_flags2_mask variable not copied to nci command buffer, this bug was added as part of AOSP 8.1 code merging for below patch:
    [artf512013][O-MR1]NCI2.0 update LF_T3T_PARAMETERS of 18 byte.

--- src/nfa/ce/nfa_ce_act.c
+++ src/nfa/ce/nfa_ce_act.c
@@ -395,7 +395,7 @@ void nfc_ce_t3t_set_listen_params(void) {
   UINT8_TO_STREAM(p_params, NFC_PMID_LF_T3T_FLAGS2);      /* type */
   UINT8_TO_STREAM(p_params, NCI_PARAM_LEN_LF_T3T_FLAGS2); /* length */
   /* Mask of IDs to disable listening */
-  UINT16_TO_STREAM(p_params, t3t_flags2_mask);
+  UINT16_TO_BE_STREAM(p_params, t3t_flags2_mask);
 
   if (NFC_GetNCIVersion() == NCI_VERSION_2_0) {
     /*Name changed in NCI2.0*/
