b08c68c0f7ba204ad4865527d29632077286196b
hariprasad nalacheruvu <hariprasad.nalacheruvu@nxp.com>
2018-08-25 12:47:39 +0530

    During SMB Ext-APDU data transceive, data bytes are transmitted in chunks of 255 instead of 258 bytes
    The HCI data packets are sent in chunks of 251 instead of 254 as result the NCI command packet size is 255 instead of
    258  this needs to be corrected.The HCI

--- SN100x/src/nfa/hci/nfa_hci_utils.cc
+++ SN100x/src/nfa/hci/nfa_hci_utils.cc
@@ -326,7 +326,11 @@ tNFA_STATUS nfa_hciu_send_msg(uint8_t pipe_id, uint8_t type,
   bool first_pkt = true;
   uint16_t data_len;
   tNFA_STATUS status = NFA_STATUS_OK;
+#if(NXP_EXTNS == TRUE)
+  uint16_t max_seg_hcp_pkt_size = nfa_hci_cb.buff_size;
+#else
   uint16_t max_seg_hcp_pkt_size = nfa_hci_cb.buff_size - NCI_DATA_HDR_SIZE;
+#endif
 
   char buff[100];
 
