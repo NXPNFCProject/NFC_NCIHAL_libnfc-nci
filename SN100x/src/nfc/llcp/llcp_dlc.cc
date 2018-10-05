56b08f54f9138e8d3cb56eefba699b5793ef8c3f
Nikhil Chhabra <nikhil.chhabra@nxp.com>
2018-10-05 14:24:58 +0530

    LLCP TC_TAR_BI_02 x=5 IUT sending DM PDU instead of SYMM PDU

--- SN100x/src/nfc/llcp/llcp_dlc.cc
+++ SN100x/src/nfc/llcp/llcp_dlc.cc
@@ -15,7 +15,25 @@
  *  limitations under the License.
  *
  ******************************************************************************/
-
+/******************************************************************************
+ *
+ *  The original Work has been changed by NXP.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Copyright 2018 NXP
+ *
+ ******************************************************************************/
 /******************************************************************************
  *
  *  This file contains the LLCP Data Link Connection Management
@@ -648,11 +666,21 @@ static void llcp_dlc_proc_connect_pdu(uint8_t dsap, uint8_t ssap,
      * i'e with improper length and service name "urn:nfc:sn:dta-co-echo-in",
      * the IUT should not send any PDU except SYMM PDU */
 
+#if (NXP_EXTNS != TRUE)
     if (appl_dta_mode_flag == 1 &&
         p_data[1] == strlen((const char*)&p_data[2])) {
       DLOG_IF(INFO, nfc_debug_enabled)
           << StringPrintf("%s: Strings are not equal", __func__);
       llcp_util_send_dm(ssap, dsap, LLCP_SAP_DM_REASON_NO_SERVICE);
+#else
+     if (appl_dta_mode_flag == 1) {
+       /* p_data is a TLV (Tag, Length, Value) */
+       if(p_data[1] == strlen((const char*)&p_data[2])) {
+         DLOG_IF(INFO, nfc_debug_enabled)
+             << StringPrintf("%s: Length & Tag fields are not equal", __func__);
+         llcp_util_send_dm(ssap, dsap, LLCP_SAP_DM_REASON_NO_SERVICE);
+       }
+#endif
     } else {
       llcp_util_send_dm(ssap, dsap, LLCP_SAP_DM_REASON_NO_SERVICE);
     }
