ab3aaffedfb2f5be9a1920ebe8cb846b31d5198d
nx35421f <sangeetha.agarwal@nxp.com>
2018-10-25 13:54:01 +0530

    MW version updated to 9.2.7

--- src/nfc/include/nfc_api.h
+++ src/nfc/include/nfc_api.h
@@ -121,7 +121,7 @@
 #define NXP_EN_PN81T     0
 #define NXP_ANDROID_VER (9U)        /* NXP android version */
 #define NFC_NXP_MW_VERSION_MAJ (0x02) /* MW Major Version */
-#define NFC_NXP_MW_VERSION_MIN (0x06) /* MW Minor Version */
+#define NFC_NXP_MW_VERSION_MIN (0x07) /* MW Minor Version */
 #endif
 /* 0xE0 ~0xFF are proprietary status codes */
 /* Command started successfully                     */
