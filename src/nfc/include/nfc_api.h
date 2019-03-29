5311dd17c1bb20daf13f53197e70c8a6643e03da
nxf24591 <nanjesh.s_1@nxp.com>
2019-03-29 16:29:42 +0530

    MW version updated to 9.6.0 and Chip type PN80T,553 & PN81T & PN557 set

--- src/nfc/include/nfc_api.h
+++ src/nfc/include/nfc_api.h
@@ -112,16 +112,16 @@
 #define NXP_EN_PN66T 0
 #define NXP_EN_PN551 0
 #define NXP_EN_PN67T 0
-#define NXP_EN_PN553 0
-#define NXP_EN_PN80T 0
+#define NXP_EN_PN553 1
+#define NXP_EN_PN80T 1
 #define NXP_EN_PN553_MR1 0
 #define NXP_EN_PN81A     0
 #define NXP_EN_PN553_MR2 0
 #define NXP_EN_PN557     1
 #define NXP_EN_PN81T     1
 #define NXP_ANDROID_VER (9U)        /* NXP android version */
-#define NFC_NXP_MW_VERSION_MAJ (0x05) /* MW Major Version */
-#define NFC_NXP_MW_VERSION_MIN (0x07) /* MW Minor Version */
+#define NFC_NXP_MW_VERSION_MAJ (0x06) /* MW Major Version */
+#define NFC_NXP_MW_VERSION_MIN (0x00) /* MW Minor Version */
 #endif
 /* 0xE0 ~0xFF are proprietary status codes */
 /* Command started successfully                     */
