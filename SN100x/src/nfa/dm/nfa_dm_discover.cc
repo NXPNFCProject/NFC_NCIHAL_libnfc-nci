4551e09505bc11eee034f3715edda8a22933cb9a
nxf32288 <ganesh.deva_1@nxp.com>
2019-04-02 16:56:31 +0530

    {Q-DP1}: Stop quick timer started during previous activation
    Quick timer used by P2P for previous activation is stopped if its
    running.

--- SN100x/src/nfa/dm/nfa_dm_discover.cc
+++ SN100x/src/nfa/dm/nfa_dm_discover.cc
@@ -3108,6 +3108,9 @@ bool nfa_dm_p2p_prio_logic(uint8_t event, uint8_t* p, uint8_t event_type) {
       DLOG_IF(INFO, nfc_debug_enabled) << StringPrintf(
           "nfa_dm_p2p_prio_logic listen mode activated reset all the "
           "nfa_dm_p2p_prio_logic variables ");
+      if (p2p_prio_logic_data.timer_list.in_use) {
+        nfc_stop_quick_timer(&p2p_prio_logic_data.timer_list);
+      }
       nfa_dm_p2p_prio_logic_cleanup();
     }
 
