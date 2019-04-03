288c6aabebccedf6cdac8b03e76c80570f2f920e
nxf32288 <ganesh.deva_1@nxp.com>
2019-04-03 11:47:25 +0530

    {Q-DP1}: Prevent integer underflow in rw_t2t_handle_tlv_detect_rsp()

--- SN100x/src/nfc/tags/rw_t2t_ndef.cc
+++ SN100x/src/nfc/tags/rw_t2t_ndef.cc
@@ -595,7 +595,12 @@ static void rw_t2t_handle_tlv_detect_rsp(uint8_t* p_data) {
             break;
 
           case TAG_LOCK_CTRL_TLV:
-            p_t2t->bytes_count--;
+            if (p_t2t->bytes_count > 0) {
+              p_t2t->bytes_count--;
+            } else {
+              LOG(ERROR) << StringPrintf("Underflow p_t2t->bytes_count!");
+              android_errorWriteLog(0x534e4554, "120506143");
+            }
             if ((tlvtype == TAG_LOCK_CTRL_TLV) || (tlvtype == TAG_NDEF_TLV)) {
               /* Collect Lock TLV */
               p_t2t->tlv_value[2 - p_t2t->bytes_count] = p_data[offset];
@@ -647,7 +652,12 @@ static void rw_t2t_handle_tlv_detect_rsp(uint8_t* p_data) {
             break;
 
           case TAG_MEM_CTRL_TLV:
-            p_t2t->bytes_count--;
+            if (p_t2t->bytes_count > 0) {
+              p_t2t->bytes_count--;
+            } else {
+              LOG(ERROR) << StringPrintf("bytes_count underflow!");
+              android_errorWriteLog(0x534e4554, "120506143");
+            }
             if ((tlvtype == TAG_MEM_CTRL_TLV) || (tlvtype == TAG_NDEF_TLV)) {
               p_t2t->tlv_value[2 - p_t2t->bytes_count] = p_data[offset];
               if (p_t2t->bytes_count == 0) {
@@ -679,7 +689,12 @@ static void rw_t2t_handle_tlv_detect_rsp(uint8_t* p_data) {
             break;
 
           case TAG_PROPRIETARY_TLV:
-            p_t2t->bytes_count--;
+            if (p_t2t->bytes_count > 0) {
+              p_t2t->bytes_count--;
+            } else {
+              LOG(ERROR) << StringPrintf("bytes_count underflow!");
+              android_errorWriteLog(0x534e4554, "120506143");
+            }
             if (tlvtype == TAG_PROPRIETARY_TLV) {
               found = true;
               p_t2t->prop_msg_len = len;
