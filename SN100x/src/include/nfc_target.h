10e1dbdc5accac35468bc4da3f76fe5fa92965b7
Suraj Uday Kotharkar <suraj.uday.kotharkar@nxp.com>
2019-04-17 21:40:37 +0530

    Routing table update is happening twice if routing table size if more than 255
            Description:
            empty AID based route is missing in routing table if 50AIDs are registered because
            AID support is Exceed with range 50 and 51 is the empty AID which will reject and not update in routing table.

--- SN100x/src/include/nfc_target.h
+++ SN100x/src/include/nfc_target.h
@@ -15,7 +15,25 @@
  *  limitations under the License.
  *
  ******************************************************************************/
-
+/******************************************************************************
+ *
+ *  The original Work has been changed by NXP.
+ *
+ *  Copyright 2019 NXP
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *
+ ******************************************************************************/
 #ifndef NFC_TARGET_H
 #define NFC_TARGET_H
 
@@ -523,7 +541,7 @@
 #ifndef NFA_EE_MAX_AID_ENTRIES
 #define NFA_EE_MIN_AID_SIZE (5)
 #define NFA_EE_MIN_AID_ENTRY_SIZE (NFA_EE_MIN_AID_SIZE + 4)
-#define NFA_EE_MAX_AID_ENTRIES (50)
+#define NFA_EE_MAX_AID_ENTRIES (51)
 #endif
 #endif
 
