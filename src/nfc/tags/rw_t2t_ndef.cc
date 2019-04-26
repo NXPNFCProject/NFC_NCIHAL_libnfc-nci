26211e1a6be4ad780c6e614f5d5fcc1f1e089209
nxf32288 <ganesh.deva_1@nxp.com>
2019-04-26 17:46:03 +0530

    {Q_DP1}: Prevent OOB error for T2T read/writes

--- src/nfc/tags/rw_t2t_ndef.cc
+++ src/nfc/tags/rw_t2t_ndef.cc
@@ -45,6 +45,7 @@
 
 #include <android-base/stringprintf.h>
 #include <base/logging.h>
+#include <log/log.h>
 
 #include "nfc_target.h"
 
@@ -647,6 +648,10 @@ static void rw_t2t_handle_tlv_detect_rsp(uint8_t* p_data) {
 
                 /* Extract lockbytes info addressed by this Lock TLV */
                 xx = 0;
+                if (count > RW_T2T_MAX_LOCK_BYTES) {
+                  count = RW_T2T_MAX_LOCK_BYTES;
+                  android_errorWriteLog(0x534e4554, "112161557");
+                }
                 while (xx < count) {
                   p_t2t->lockbyte[p_t2t->num_lockbytes].tlv_index =
                       p_t2t->num_lock_tlvs;
