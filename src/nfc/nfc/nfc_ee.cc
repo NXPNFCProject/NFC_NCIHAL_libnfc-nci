60bf0bcb237b54ce2713cddf281bd080fa46d89f
nxf48293 <priyanka.rajput@nxp.com>
2019-05-02 18:21:28 +0530

    Compilation Fix

--- src/nfc/nfc/nfc_ee.cc
+++ src/nfc/nfc/nfc_ee.cc
@@ -85,7 +85,6 @@ tNFC_STATUS NFC_NfceeModeSet(uint8_t nfcee_id, tNFC_NFCEE_MODE mode) {
   if ((nfc_cb.nci_version != NCI_VERSION_2_0) &&
       ((nfcFL.chipType != pn553) && (nfcFL.chipType != pn80T))) {
       status = nci_snd_nfcee_mode_set(nfcee_id, mode);
-    }
   } else {
     if (nfc_cb.flags & NFC_FL_WAIT_MODE_SET_NTF)
       status = NFC_STATUS_REFUSED;
