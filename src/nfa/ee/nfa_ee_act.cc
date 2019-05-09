2e94a54181a32d9acbe2f59a392b05893e4a59b8
nxf32288 <ganesh.deva_1@nxp.com>
2019-05-09 16:04:31 +0530

    Fix for wrong ISO-DEP route power_state during Secure NFC on.

--- src/nfa/ee/nfa_ee_act.cc
+++ src/nfa/ee/nfa_ee_act.cc
@@ -497,12 +497,17 @@ static void nfa_ee_add_proto_route_to_ecb(tNFA_EE_ECB* p_cb, uint8_t* pp,
         proto_tag = NFC_ROUTE_TAG_PROTO | nfa_ee_cb.route_block_control;
 
 #if(NXP_EXTNS == TRUE)
+        /* Enable screen on lock power state for ISO-DEP protocol to
+           enable HCE screen lock */
         if (p_cb->proto_screen_lock & nfa_ee_proto_mask_list[xx])
-           power_cfg |= NCI_ROUTE_PWR_STATE_SCREEN_ON_LOCK();
-        if (p_cb->proto_screen_off & nfa_ee_proto_mask_list[xx])
-           power_cfg |= NCI_ROUTE_PWR_STATE_SCREEN_OFF_UNLOCK();
-        if (p_cb->proto_screen_off_lock & nfa_ee_proto_mask_list[xx])
-           power_cfg |= NCI_ROUTE_PWR_STATE_SCREEN_OFF_LOCK();
+          power_cfg |= NCI_ROUTE_PWR_STATE_SCREEN_ON_LOCK();
+
+        if (p_cb->nfcee_id != NFC_DH_ID) {
+          if (p_cb->proto_screen_off & nfa_ee_proto_mask_list[xx])
+             power_cfg |= NCI_ROUTE_PWR_STATE_SCREEN_OFF_UNLOCK();
+          if (p_cb->proto_screen_off_lock & nfa_ee_proto_mask_list[xx])
+             power_cfg |= NCI_ROUTE_PWR_STATE_SCREEN_OFF_LOCK();
+         }
 #else
         if ((power_cfg & NCI_ROUTE_PWR_STATE_ON) &&
             (NFC_GetNCIVersion() == NCI_VERSION_2_0)) {
