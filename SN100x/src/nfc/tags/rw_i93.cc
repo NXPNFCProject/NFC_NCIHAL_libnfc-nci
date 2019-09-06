40f92798b058450e0e1724f2aa1559760d518e34
Lijo Abraham <lijon.abraham@nxp.com>
2019-09-06 14:56:55 +0530

    Prevent OOB read in rw_i93_sm_format()

--- SN100x/src/nfc/tags/rw_i93.cc
+++ SN100x/src/nfc/tags/rw_i93.cc
@@ -2415,6 +2415,10 @@ void rw_i93_sm_format(NFC_HDR* p_resp) {
       "sub_state:%s (0x%x)",
       rw_i93_get_sub_state_name(p_i93->sub_state).c_str(), p_i93->sub_state);
 
+  if (length == 0) {
+    android_errorWriteLog(0x534e4554, "122323053");
+    return;
+  }
   STREAM_TO_UINT8(flags, p);
   length--;
 
@@ -2441,6 +2445,10 @@ void rw_i93_sm_format(NFC_HDR* p_resp) {
   switch (p_i93->sub_state) {
     case RW_I93_SUBSTATE_WAIT_UID:
 
+      if (length < (I93_UID_BYTE_LEN + 1)) {
+        android_errorWriteLog(0x534e4554, "122323053");
+        return;
+      }
       p++; /* skip DSFID */
       p_uid = p_i93->uid;
       STREAM_TO_ARRAY8(p_uid, p); /* store UID */
