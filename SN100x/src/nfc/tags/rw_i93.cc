78ba85a060cf4a9cb4d3c811fe611fdeaf896807
nxf55720 <lijon.abraham@nxp.com>
2019-11-15 19:09:04 +0530

    Prevent OOB error in rw_i93_sm_read_ndef() (cherry picked from commit fcf05e6afa09da34790447c33563e26a2470d15e)
    Change-Id: Ia6c6475e459361cd36e8b54e0d26a01cac47cae8

--- SN100x/src/nfc/tags/rw_i93.cc
+++ SN100x/src/nfc/tags/rw_i93.cc
@@ -2068,6 +2068,7 @@ void rw_i93_sm_read_ndef(NFC_HDR* p_resp) {
 
   if (length == 0) {
     android_errorWriteLog(0x534e4554, "122035770");
+    rw_i93_handle_error(NFC_STATUS_FAILED);
     return;
   }
 
