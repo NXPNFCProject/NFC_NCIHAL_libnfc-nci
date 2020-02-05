8c02424e915848c397e6f67d8f8cf810197ed328
Nanjesh Shivalingaiah <nanjesh.s_1@nxp.com>
2020-02-05 06:27:24 +0100

    MW version updated to 10.00.11 RC1
    Change-Id: Iee518377195e145ef0663d57b1ad34b41abcec9c

--- SN100x/src/nfc/include/nfc_api.h
+++ SN100x/src/nfc/include/nfc_api.h
@@ -57,7 +57,7 @@
 #define NXP_EN_SN100U    1
 #define NXP_ANDROID_VER (10U)        /* NXP android version */
 #define NFC_NXP_MW_VERSION_MAJ (0x00) /* MW Major Version */
-#define NFC_NXP_MW_VERSION_MIN (0x10) /* MW Minor Version */
+#define NFC_NXP_MW_VERSION_MIN (0x11) /* MW Minor Version */
 #define NFC_NXP_MW_CUSTOMER_ID (0x00) /* MW Customer Id */
 #define NFC_NXP_MW_RC_VERSION  (0x01) /* MW RC Version */
 #define NFC_EE_DISC_OP_REMOVE 1
