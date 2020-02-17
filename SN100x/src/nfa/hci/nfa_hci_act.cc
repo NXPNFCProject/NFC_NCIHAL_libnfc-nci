605608aba0045a8d1cceaa3fb5ab981aac7fd70e
nxf52135 <amit.sharma_1@nxp.com>
2020-02-17 18:57:22 +0530

    {R-DP1}Reset the HCI network if nfaStorage was deleted.
    If nfaStroage.bin1 file cannot be opened, send a ADM_CLEAR_ALL_PIPE command
    to force the NFCC to recreate all the pipes.
    
    Bug: 137065908

--- SN100x/src/nfa/hci/nfa_hci_act.cc
+++ SN100x/src/nfa/hci/nfa_hci_act.cc
@@ -1579,7 +1579,7 @@ void nfa_hci_handle_admin_gate_rsp(uint8_t* p_data, uint8_t data_len) {
             /* Something wrong, NVRAM data could be corrupt or first start with
              * default session id */
             nfa_hciu_send_clear_all_pipe_cmd();
-            nfa_hci_cb.b_hci_netwk_reset = true;
+            nfa_hci_cb.b_hci_new_sessionId = true;
             if (data_len < NFA_HCI_SESSION_ID_LEN) {
               android_errorWriteLog(0x534e4554, "124524315");
             }
@@ -1591,7 +1591,14 @@ void nfa_hci_handle_admin_gate_rsp(uint8_t* p_data, uint8_t data_len) {
         nfa_hci_cb.cfg.admin_gate.pipe01_state = NFA_HCI_PIPE_OPENED;
 
         if (nfa_hci_cb.b_hci_netwk_reset) {
+          /* Something wrong, NVRAM data could be corrupt or first start with
+           * default session id */
+          nfa_hciu_send_clear_all_pipe_cmd();
           nfa_hci_cb.b_hci_netwk_reset = false;
+          nfa_hci_cb.b_hci_new_sessionId = true;
+        } else if (nfa_hci_cb.b_hci_new_sessionId) {
+          nfa_hci_cb.b_hci_new_sessionId = false;
+
           /* Session ID is reset, Set New session id */
           memcpy(
               &nfa_hci_cb.cfg.admin_gate.session_id[NFA_HCI_SESSION_ID_LEN / 2],
