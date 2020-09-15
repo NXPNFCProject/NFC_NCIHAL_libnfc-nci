3e96e1f73b3d926f62bba3442b61b1ff24494f33
Ashish Manohar <ashish.manohar@nxp.com>
2020-09-15 12:30:51 +0530

    Nxp Copyright added for nxp_nci_hal_libnfc-nci module
    Nxp Copyright  is added.
    Nxp Copyright year is updated for modified file.

--- SN100x/src/nfc/tags/ce_t4t.cc
+++ SN100x/src/nfc/tags/ce_t4t.cc
@@ -16,8 +16,9 @@
  *
  ******************************************************************************/
 /******************************************************************************
+ *  The original Work has been changed by NXP.
  *
- *  Copyright 2019 NXP
+ *  Copyright 2019-2020 NXP
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
