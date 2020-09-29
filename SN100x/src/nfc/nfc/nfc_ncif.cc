1042c5bb6d601882526c88e32ddec0f98ab05e50
nxf32288 <ganesh.deva_1@nxp.com>
2020-09-29 10:18:49 +0530

    Store crash NCI log before reporting error status
    NativeNfcManager would also try to abort nfc stack when receiving,
    which cause the native crash logs could not be stored before aborting.

--- SN100x/src/nfc/nfc/nfc_ncif.cc
+++ SN100x/src/nfc/nfc/nfc_ncif.cc
@@ -177,6 +177,8 @@ void nfc_ncif_update_window(void) {
 void nfc_ncif_cmd_timeout(void) {
   LOG(ERROR) << StringPrintf("nfc_ncif_cmd_timeout");
 
+  storeNativeCrashLogs();
+
   /* report an error */
   nfc_ncif_event_status(NFC_GEN_ERROR_REVT, NFC_STATUS_HW_TIMEOUT);
   nfc_ncif_event_status(NFC_NFCC_TIMEOUT_REVT, NFC_STATUS_HW_TIMEOUT);
@@ -187,7 +189,6 @@ void nfc_ncif_cmd_timeout(void) {
   }
 
   /* XXX maco since this failure is unrecoverable, abort the process */
-  storeNativeCrashLogs();
   abort();
 }
 
