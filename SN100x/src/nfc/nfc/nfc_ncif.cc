04659316c96019c88aa616d2c3f878c666e23a0c
nxf63550 <bhautik.ardeshana@nxp.com>
2020-10-05 19:34:32 +0530

    Fix data exchange not working in active P2P mode
    - While MW parsing the RF_INTF_ACT_NTF, not handled the case if
      Length of RF Technology Specific Parameters is Zero.

--- SN100x/src/nfc/nfc/nfc_ncif.cc
+++ SN100x/src/nfc/nfc/nfc_ncif.cc
@@ -958,18 +958,21 @@ Available after Technology Detection
   } else if (NCI_DISCOVERY_TYPE_POLL_ACTIVE == p_param->mode) {
     acm_p = &p_param->param.acm_p;
 
+#if (NXP_EXTNS == TRUE)
+    if(plen == 0) {
+      /*No RF Technology specific parameters, skipping next 3 bytes
+       * Byte 1         Byte 2 Byte 3  Byte 4
+       * Tech and Mode  BST    BRT     offset*/
+      p = p + 3;
+      plen = *p++;
+      LOG(INFO) << StringPrintf("Zero Length of RF Technology Specific Parameters,"
+                                " plen: 0x%x, atr_res_len: 0x%x", plen, *p);
+    }
+#endif
     if (plen < 1) {
       goto invalid_packet;
     }
     plen--;
-#if (NXP_EXTNS == TRUE)
-    if(len == 0) {
-      /*No RF Technology specific parameters, skipping next 3 bytes
-       * Byte 1         Byte 2 Byte 3  Byte 4
-       * Tech and Mode  BST    BRT     offset*/
-      p = p + 4;
-    }
-#endif
     acm_p->atr_res_len = *p++;
     if (acm_p->atr_res_len > 0) {
       if (acm_p->atr_res_len > NFC_MAX_ATS_LEN)
