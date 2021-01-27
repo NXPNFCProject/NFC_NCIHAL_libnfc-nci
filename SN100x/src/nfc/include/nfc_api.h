18c209768750fd17ad258c059083535b7181828b
Nanjesh Shivalingaiah <nanjesh.s_1@nxp.com>
2021-01-27 15:49:13 +0100

    MW version updated to 12.00.02 (SN220) & 12.50.01 (SN1xx) CBC version updated to 1.2
    Change-Id: I368e20c13a15722b6f05cc232b190576ef36ac35

--- SN100x/src/nfc/include/nfc_api.h
+++ SN100x/src/nfc/include/nfc_api.h
@@ -61,13 +61,13 @@
 #define NFC_NXP_MW_RC_VERSION  (0x00) /* MW RC Version */
 
 #define NFC_NXP_CBC_VERSION_MAJ (0x01) /* CBC Major Version */
-#define NFC_NXP_CBC_VERSION_MIN (0x01) /* CBC Minor Version */
+#define NFC_NXP_CBC_VERSION_MIN (0x02) /* CBC Minor Version */
 
-#define NFC_NXP_MW_SN1XX_VERSION_MAJ (0x00) /* MW Major Version for SN1XX*/
+#define NFC_NXP_MW_SN1XX_VERSION_MAJ (0x50) /* MW Major Version for SN1XX*/
 #define NFC_NXP_MW_SN1XX_VERSION_MIN (0x01) /* MW Minor Version for SN1xx*/
 
 #define NFC_NXP_MW_SN220_VERSION_MAJ (0x00) /* MW Major Version for SN220*/
-#define NFC_NXP_MW_SN220_VERSION_MIN (0x01) /* MW Minor Version for SN220*/
+#define NFC_NXP_MW_SN220_VERSION_MIN (0x02) /* MW Minor Version for SN220*/
 #define NFC_EE_DISC_OP_REMOVE 1
 #endif
 /* NFC application return status codes */
