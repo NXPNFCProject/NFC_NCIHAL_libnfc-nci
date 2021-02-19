90849780cd6f5b64a3d4f5c013bf7274ca5476ad
Bhautik Ardeshana <bhautik.ardeshana@nxp.com>
2021-02-19 18:05:15 +0530

    Fix T4T Ndef write and read api is failing contents mismatch
    - Updated nlen_size value as T4T_FILE_LENGTH_SIZE

--- SN100x/src/nfc/tags/rw_t4t.cc
+++ SN100x/src/nfc/tags/rw_t4t.cc
@@ -31,7 +31,7 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 *
-*  Copyright 2018-2020 NXP
+*  Copyright 2018-2021 NXP
 *
 ******************************************************************************/
 
@@ -2658,6 +2658,10 @@ tNFC_STATUS RW_T4tNfceeReadPendingData() {
 *******************************************************************************/
 tNFC_STATUS RW_T4tNfceeUpdateNlen(uint16_t len) {
   DLOG_IF(INFO, nfc_debug_enabled) << StringPrintf("%s Enter ", __func__);
+  /* update nlen_size with T4T_FILE_LENGTH_SIZE to avoid mismatch in
+   * reading/writing length of data*/
+  tRW_T4T_CB* p_t4t = &rw_cb.tcb.t4t;
+  p_t4t->cc_file.ndef_fc.nlen_size = T4T_FILE_LENGTH_SIZE;
   if (!rw_t4t_update_nlen(len)) {
     return NFC_STATUS_FAILED;
   }
