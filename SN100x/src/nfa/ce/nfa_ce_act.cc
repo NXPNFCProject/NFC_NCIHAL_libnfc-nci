605335c1c64e17374730d41d3a0e8429a28f79d4
nxf66043 <yuvaraja.manikandan@nxp.com>
2021-03-17 14:13:37 +0530

    Copyright banner updated

--- SN100x/src/nfa/ce/nfa_ce_act.cc
+++ SN100x/src/nfa/ce/nfa_ce_act.cc
@@ -31,7 +31,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  Copyright 2020 NXP
+ *  Copyright 2020-2021 NXP
  *
  ******************************************************************************/
 
