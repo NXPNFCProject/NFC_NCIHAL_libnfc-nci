471318f0a1c8f829c2b3d38be837c21135d85b1c
nxf56319 <sai.shwethas@nxp.com>
2021-05-24 14:03:30 +0530

    Add missing NCI config parameters definitions
    Compliant with latest NCI2.1
    
    Bug: 169640640
    Test: Tag R/W
    Change-Id: Ib8bf1503336dae63bd11c6309c9d7943bca2ad15

--- src/nfa/include/nfa_api.h
+++ src/nfa/include/nfa_api.h
@@ -78,6 +78,10 @@
 #define NFA_STATUS_INVALID_PARAM NCI_STATUS_INVALID_PARAM
 /* Already started      */
 #define NFA_STATUS_ALREADY_STARTED NCI_STATUS_ALREADY_STARTED
+/* RF frame error       */
+#define NFA_STATUS_RF_FRAME_CORRUPTED NCI_STATUS_RF_FRAME_CORRUPTED
+/* RF protocol error    */
+#define NFA_STATUS_RF_PROTOCOL_ERR NCI_STATUS_RF_PROTOCOL_ERR
 /* RF Timeout           */
 #define NFA_STATUS_TIMEOUT NCI_STATUS_TIMEOUT
 /* not initialized      */
