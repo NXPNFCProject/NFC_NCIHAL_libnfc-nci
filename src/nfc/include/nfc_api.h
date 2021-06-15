4caf486aedf661b72fb9209ae59526a922a111aa
nxf32288 <ganesh.deva_1@nxp.com>
2021-06-15 19:15:31 +0530

    MW version print updated.

--- src/nfc/include/nfc_api.h
+++ src/nfc/include/nfc_api.h
@@ -119,8 +119,8 @@
 #define NXP_EN_PN553_MR2 0
 #define NXP_EN_PN557     1
 #define NXP_EN_PN81T     1
-#define NXP_ANDROID_VER (11U)        /* NXP android version */
-#define NFC_NXP_MW_VERSION_MAJ (0x03) /* MW Major Version */
+#define NXP_ANDROID_VER (12U)        /* NXP android version */
+#define NFC_NXP_MW_VERSION_MAJ (0x00) /* MW Major Version */
 #define NFC_NXP_MW_VERSION_MIN (0x01) /* MW Minor Version */
 #define NFC_NXP_MW_CUSTOMER_ID (0x00) /* MW Customer Id */
 #endif
