0bac4e127525798a1207f9ae3769e3a355cf38df
nxf36763 <ashish.manohar@nxp.com>
2021-12-06 14:44:34 +0530

    Initial NFC fuzzer projects checkin
    5 fuzzers are included:nci packet handler, tag read/write code,
        card emulation code, Ndef and llcp.
    
    This is code drop from AOSP master system nfc module
    [commit Id- 6e44d60bb682a3b5aa3399ebe5cffef007dc387e]
    Bug: 176237383
    
    Change-Id: I7eee39e8831b9a5eef36fbc8a72818bf84f05dd6
    Test: Manually ran affected NFC scenarios.

---
+++ SN100x/src/fuzzers/inc/statslog.h
@@ -0,0 +1,20 @@
+#ifndef __STATS_LOG_H__
+#define __STATS_LOG_H__
+
+#include <log/log.h>
+#include <sys/time.h>
+
+// Dummy definitions to get rid of unnecessary dependency
+
+namespace android {
+namespace util {
+enum {
+  NFC_HCE_TRANSACTION_OCCURRED,
+  NFC_ERROR_OCCURRED,
+};
+
+void stats_write(int32_t, ...) {}
+}  // namespace util
+}  // namespace android
+
+#endif
