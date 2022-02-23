f5451391501292ed19840136441e7641e78417dd
nxf70628 <juffinalex.varghese@nxp.com>
2022-02-23 12:46:30 +0530

    Tag-mismatch in NFA_SendRawFrame
    CVE ID: CVE-2021-39675 (FEB 2022)

--- SN100x/src/gki/common/gki_buffer.cc
+++ SN100x/src/gki/common/gki_buffer.cc
@@ -31,11 +31,12 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 *
-*  Copyright 2020-2021 NXP
+*  Copyright 2020-2022 NXP
 *
 ******************************************************************************/
 #include <android-base/stringprintf.h>
 #include <base/logging.h>
+#include <log/log.h>
 #include "gki_int.h"
 
 #if (GKI_NUM_TOTAL_BUF_POOLS > 16)
@@ -279,8 +280,9 @@ void* GKI_getbuf(uint16_t size) {
   FREE_QUEUE_T* Q;
 
 #if defined(DYN_ALLOC) || defined(FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION)
-  if (size == 0) {
-    LOG(ERROR) << StringPrintf("getbuf: Size is zero");
+  if (size == 0 || size > (USHRT_MAX - 3)) {
+    LOG(ERROR) << StringPrintf("getbuf: Requested size(%d) is invalid", size);
+    android_errorWriteLog(0x534e4554, "205729183");
 #ifndef DYN_ALLOC
     abort();
 #else
