abf353845e0bd2eba403739834dcb144ccc2074c
nxf56319 <sai.shwethas@nxp.com>
2022-05-02 16:16:51 +0530

    nfc: dynamic memory allocation
    The static buffer pool is disabled and
     the buffers are allocated on demand.
    Only buffer allocation mechanism is affected no attempts to
     optimize memory allocations in the code.
    Basic memory leak check is also included for testing.
    
    Bug: 146228323
    Test: runs ok, no obvious regressions
    
    Change-Id: I51e51eac1d5dbeb89cb2ef80c055d7ea148d8e13

--- src/gki/common/gki_buffer.cc
+++ src/gki/common/gki_buffer.cc
@@ -28,6 +28,8 @@ static void gki_add_to_pool_list(uint8_t pool_id);
 static void gki_remove_from_pool_list(uint8_t pool_id);
 #endif /*  BTU_STACK_LITE_ENABLED == false */
 
+extern bool nfc_debug_enabled;
+
 using android::base::StringPrintf;
 
 /*******************************************************************************
@@ -252,9 +254,61 @@ void GKI_init_q(BUFFER_Q* p_q) {
 **
 *******************************************************************************/
 void* GKI_getbuf(uint16_t size) {
-  uint8_t i;
-  FREE_QUEUE_T* Q;
   BUFFER_HDR_T* p_hdr;
+  FREE_QUEUE_T* Q;
+
+#if defined(DYN_ALLOC) || defined(FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION)
+  if (size == 0) {
+    LOG(ERROR) << StringPrintf("getbuf: Size is zero");
+#ifndef DYN_ALLOC
+    abort();
+#else
+    return (nullptr);
+#endif
+  }
+
+  size = ALIGN_POOL(size);
+  size_t total_sz = size + sizeof(BUFFER_HDR_T)
+#if (GKI_ENABLE_BUF_CORRUPTION_CHECK == TRUE)
+                    + sizeof(uint32_t);
+#else
+      ;
+#endif
+  p_hdr = (BUFFER_HDR_T*)GKI_os_malloc(total_sz);
+  if (!p_hdr) {
+    LOG(ERROR) << StringPrintf("unable to allocate buffer!!!!!");
+#ifndef DYN_ALLOC
+    abort();
+#else
+    return (nullptr);
+#endif
+  }
+
+  memset(p_hdr, 0, total_sz);
+
+#if (GKI_ENABLE_BUF_CORRUPTION_CHECK == TRUE)
+  *(uint32_t*)((uint8_t*)p_hdr + BUFFER_HDR_SIZE + size) = MAGIC_NO;
+#endif
+  p_hdr->task_id = GKI_get_taskid();
+  p_hdr->status = BUF_STATUS_UNLINKED;
+  p_hdr->p_next = nullptr;
+  p_hdr->Type = 0;
+
+  p_hdr->q_id = 0;
+  p_hdr->size = size;
+
+  GKI_disable();
+  Q = &gki_cb.com.freeq[p_hdr->q_id];
+  if (++Q->cur_cnt > Q->max_cnt) Q->max_cnt = Q->cur_cnt;
+  GKI_enable();
+
+  DLOG_IF(INFO, nfc_debug_enabled) << StringPrintf(
+      "%s %p %d:%d", __func__, ((uint8_t*)p_hdr + BUFFER_HDR_SIZE), Q->cur_cnt,
+      Q->max_cnt);
+  UNUSED(gki_alloc_free_queue);
+  return (void*)((uint8_t*)p_hdr + BUFFER_HDR_SIZE);
+#else
+  uint8_t i;
   tGKI_COM_CB* p_cb = &gki_cb.com;
 
   if (size == 0) {
@@ -319,6 +373,7 @@ void* GKI_getbuf(uint16_t size) {
   GKI_enable();
 
   return (nullptr);
+#endif
 }
 
 /*******************************************************************************
@@ -338,6 +393,32 @@ void* GKI_getbuf(uint16_t size) {
 **
 *******************************************************************************/
 void* GKI_getpoolbuf(uint8_t pool_id) {
+#if defined(DYN_ALLOC) || defined(FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION)
+  uint16_t size = 0;
+  switch (pool_id) {
+    // NFC_NCI_POOL_ID, NFC_RW_POOL_ID and NFC_CE_POOL_ID are all redefined to
+    // GKI_POOL_ID_2.
+    case GKI_POOL_ID_2:
+      size = GKI_BUF2_SIZE;
+      break;
+
+    // LLCP_POOL_ID, GKI_MAX_BUF_SIZE_POOL_ID are redefined to GKI_POOL_ID_3.
+    case GKI_POOL_ID_3:
+      size = GKI_BUF3_SIZE;
+      break;
+
+    default:
+      LOG(ERROR) << StringPrintf("Unknown pool ID: %d", pool_id);
+#ifndef DYN_ALLOC
+      abort();
+#else
+      return (nullptr);
+#endif
+      break;
+  }
+
+  return GKI_getbuf(size);
+#else
   FREE_QUEUE_T* Q;
   BUFFER_HDR_T* p_hdr;
   tGKI_COM_CB* p_cb = &gki_cb.com;
@@ -380,6 +461,7 @@ void* GKI_getpoolbuf(uint8_t pool_id) {
 
   /* try for free buffers in public pools */
   return (GKI_getbuf(p_cb->freeq[pool_id].size));
+#endif
 }
 
 /*******************************************************************************
@@ -417,6 +499,14 @@ void GKI_freebuf(void* p_buf) {
     return;
   }
 
+#if defined(DYN_ALLOC) || defined(FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION)
+  GKI_disable();
+  Q = &gki_cb.com.freeq[p_hdr->q_id];
+  if (Q->cur_cnt > 0) Q->cur_cnt--;
+  GKI_enable();
+
+  GKI_os_free(p_hdr);
+#else
   GKI_disable();
 
   /*
@@ -435,8 +525,7 @@ void GKI_freebuf(void* p_buf) {
   if (Q->cur_cnt > 0) Q->cur_cnt--;
 
   GKI_enable();
-
-  return;
+#endif
 }
 
 /*******************************************************************************
@@ -456,15 +545,17 @@ uint16_t GKI_get_buf_size(void* p_buf) {
 
   p_hdr = (BUFFER_HDR_T*)((uint8_t*)p_buf - BUFFER_HDR_SIZE);
 
-  if ((uintptr_t)p_hdr & 1){
-    return (0);
-  }
+#if defined(DYN_ALLOC) || defined(FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION)
+  return p_hdr->size;
+#else
+  if ((uintptr_t)p_hdr & 1) return (0);
 
   if (p_hdr->q_id < GKI_NUM_TOTAL_BUF_POOLS) {
     return (gki_cb.com.freeq[p_hdr->q_id].size);
   }
 
   return (0);
+#endif
 }
 
 /*******************************************************************************
--- src/include/buildcfg.h
+++ src/include/buildcfg.h
@@ -71,7 +71,11 @@
 #if (NXP_EXTNS == TRUE)
 #define GKI_NUM_FIXED_BUF_POOLS 5
 #else
+#ifdef DYN_ALLOC
+#define GKI_NUM_FIXED_BUF_POOLS 0
+#else
 #define GKI_NUM_FIXED_BUF_POOLS 4
+#endif
 #endif
 
 void initializeGlobalAppDtaMode();
