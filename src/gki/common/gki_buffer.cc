d04c7d8e73ab8ad581e86dd20a45699a97dfe787
nxf56319 <sai.shwethas@nxp.com>
2022-05-12 14:41:37 +0530

    P2P - URL/Contact/image transfer is not happening
    In case of dynamic memory allocation, MW shall make sure that the pool
    count is non-zero. The zero count means, no tx buffer available for comm'n.
    The data link will be considered as congested and P2P transfer will fail.

--- src/gki/common/gki_buffer.cc
+++ src/gki/common/gki_buffer.cc
@@ -15,6 +15,23 @@
  *  limitations under the License.
  *
  ******************************************************************************/
+ /******************************************************************************
+ *
+ *  Copyright 2022 NXP
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ ******************************************************************************/
 #include <android-base/stringprintf.h>
 #include <base/logging.h>
 #include <log/log.h>
@@ -1090,6 +1107,34 @@ static void gki_remove_from_pool_list(uint8_t pool_id) {
 **
 *******************************************************************************/
 uint16_t GKI_poolcount(uint8_t pool_id) {
+
+#ifdef DYN_ALLOC
+#if (NXP_EXTNS == TRUE)
+    uint16_t count = 0;
+    switch (pool_id) {
+    // NFC_NCI_POOL_ID, NFC_RW_POOL_ID and NFC_CE_POOL_ID are all redefined to
+    // GKI_POOL_ID_2.
+    case GKI_POOL_ID_2:
+      count = GKI_BUF2_SIZE;
+      break;
+
+    // LLCP_POOL_ID, GKI_MAX_BUF_SIZE_POOL_ID are redefined to GKI_POOL_ID_3.
+    case GKI_POOL_ID_3:
+      count = GKI_BUF3_SIZE;
+      break;
+
+    case GKI_POOL_ID_4:
+      count = GKI_BUF4_SIZE;
+      break;
+
+    default:
+      LOG(ERROR) << StringPrintf("Unknown pool ID: %d", pool_id);
+      count = GKI_MAX_BUF_SIZE;
+      break;
+  }
+  return count;
+#endif
+#endif
   if (pool_id >= GKI_NUM_TOTAL_BUF_POOLS) return (0);
 
   return (gki_cb.com.freeq[pool_id].total);
