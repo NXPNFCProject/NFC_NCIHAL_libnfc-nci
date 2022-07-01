2d57d51aae9c138f43db63436d6ca2484b63ff96
Nanjesh Shivalingaiah <nanjesh.s_1@nxp.com>
2022-07-01 09:20:42 +0200

    MW version updated to 13.01.00
    Change-Id: Ib1e00cb43e1e29e6a64a5325d65028ad842aadb7

--- src/nfc/include/nfc_api.h
+++ src/nfc/include/nfc_api.h
@@ -120,8 +120,8 @@
 #define NXP_EN_PN557     1
 #define NXP_EN_PN81T     1
 #define NXP_ANDROID_VER (13U)        /* NXP android version */
-#define NFC_NXP_MW_VERSION_MAJ (0x00) /* MW Major Version */
-#define NFC_NXP_MW_VERSION_MIN (0x01) /* MW Minor Version */
+#define NFC_NXP_MW_VERSION_MAJ (0x01) /* MW Major Version */
+#define NFC_NXP_MW_VERSION_MIN (0x00) /* MW Minor Version */
 #define NFC_NXP_MW_CUSTOMER_ID (0x00) /* MW Customer Id */
 #endif
 /* 0xE0 ~0xFF are proprietary status codes */
