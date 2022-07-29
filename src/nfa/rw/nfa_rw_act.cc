c94ea5fd31c794daa16a1c9a295c84acb446cdfb
nxf56319 <sai.shwethas@nxp.com>
2022-07-29 13:12:37 +0530

    Fix OOB and code defects in T5T module
    - Fix OOB memory access beyond allocated memory
    	- Modified to use i93 UID array
    - Endianness for Quiet API has been changed
    	- StayQuiet API expects different endianness of UID
    	  (LSB to MSB UID for Quiet API)
    - Fix rw_i93_handle_error for I93_CMD_STAY_QUIET command timeout
    	Change is done to make sure RF state is not moved to IDLE in case of timeout.
    
    Bug ID: 210572605
    Test: Tag R/W
    Change-Id: Ie0ae98e948ed7784f796bd5d0ef04cd6efff2138

--- src/nfa/rw/nfa_rw_act.cc
+++ src/nfa/rw/nfa_rw_act.cc
@@ -2454,7 +2454,7 @@ static bool nfa_rw_i93_command(tNFA_RW_MSG* p_data) {
 
     case NFA_RW_OP_I93_STAY_QUIET:
       i93_command = I93_CMD_STAY_QUIET;
-      status = RW_I93StayQuiet(p_data->op_req.params.i93_cmd.p_data);
+      status = RW_I93StayQuiet(p_data->op_req.params.i93_cmd.uid);
       break;
 
     case NFA_RW_OP_I93_READ_SINGLE_BLOCK:
--- src/nfa/rw/nfa_rw_api.cc
+++ src/nfa/rw/nfa_rw_api.cc
@@ -923,8 +923,8 @@ tNFA_STATUS NFA_RwI93StayQuiet(uint8_t* p_uid) {
     /* Fill in tNFA_RW_OPERATION struct */
     p_msg->hdr.event = NFA_RW_OP_REQUEST_EVT;
     p_msg->op = NFA_RW_OP_I93_STAY_QUIET;
-    p_msg->params.i93_cmd.p_data = (uint8_t*)(p_msg + 1);
-    memcpy(p_msg->params.i93_cmd.p_data, p_uid, I93_UID_BYTE_LEN);
+    p_msg->params.i93_cmd.uid_present = true;
+    memcpy(p_msg->params.i93_cmd.uid, p_uid, I93_UID_BYTE_LEN);
 
     nfa_sys_sendmsg(p_msg);
 
