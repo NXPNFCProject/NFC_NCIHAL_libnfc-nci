fba528816f6e4c6a9d1bad69e0c8a502bbfb9978
nxf31698 <suryaprakash.konduru@nxp.com>
2023-03-12 11:35:09 +0530

    Set NFC vendor logging level according to system property value
    Bug: 204397062
    Test: manual

--- SN100x/src/adaptation/NfcAdaptation.cc
+++ SN100x/src/adaptation/NfcAdaptation.cc
@@ -44,6 +44,7 @@
 // aidl => syslog.h
 #undef LOG_INFO
 #undef LOG_WARNING
+#include <android-base/properties.h>
 #include <android-base/stringprintf.h>
 #include <android/hardware/nfc/1.1/INfc.h>
 #include <android/hardware/nfc/1.2/INfc.h>
@@ -95,6 +96,10 @@ using NfcAidlEvent = ::aidl::android::hardware::nfc::NfcEvent;
 using NfcAidlStatus = ::aidl::android::hardware::nfc::NfcStatus;
 using ::aidl::android::hardware::nfc::NfcCloseType;
 using Status = ::ndk::ScopedAStatus;
+
+#define VERBOSE_VENDOR_LOG_PROPERTY "persist.nfc.verbosevendorlog"
+#define VERBOSE_VENDOR_LOG_ENABLED "enabled"
+#define VERBOSE_VENDOR_LOG_DISABLED "disabled"
 
 std::string NFC_AIDL_HAL_SERVICE_NAME = "android.hardware.nfc.INfc/default";
 #if (NXP_EXTNS == TRUE)
@@ -931,6 +936,15 @@ void NfcAdaptation::HalOpen(tHAL_NFC_CBACK* p_hal_cback,
                  << ::aidl::android::hardware::nfc::toString(
                         static_cast<NfcAidlStatus>(
                             status.getServiceSpecificError()));
+    } else {
+      bool verbose_vendor_log =
+          android::base::GetProperty(VERBOSE_VENDOR_LOG_PROPERTY, "")
+                  .compare(VERBOSE_VENDOR_LOG_ENABLED)
+              ? false
+              : true;
+      mAidlHal->setEnableVerboseLogging(verbose_vendor_log);
+      DLOG_IF(INFO, nfc_debug_enabled) << StringPrintf(
+          "%s: verbose_vendor_log=%u", __func__, verbose_vendor_log);
     }
   } else if (mHal_1_1 != nullptr) {
     mCallback = new NfcClientCallback(p_hal_cback, p_data_cback);
