e173760144fa6d6adcd06bfa2ca756f08970cbc7
nxf58888 <himanshusingh.kushwah@nxp.com>
2023-04-05 15:50:17 +0530

    Enable chipset bit for new chiptype in MW version
    Change-Id: I32f9cdb586bb792c642db92c244c8c73d704dd15

--- SN100x/src/nfa/dm/nfa_dm_api.cc
+++ SN100x/src/nfa/dm/nfa_dm_api.cc
@@ -31,7 +31,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  Copyright 2018-2022 NXP
+ *  Copyright 2018-2023 NXP
  *
  ******************************************************************************/
 
@@ -1546,6 +1546,7 @@ tNFA_MW_VERSION NFA_GetMwVersion() {
   mwVer.validation |= (NXP_EN_SN110U << 14);
   mwVer.validation |= (NXP_EN_SN220U << 15);
   mwVer.validation |= (NXP_EN_PN560 << 16);
+  mwVer.validation |= (NXP_EN_SN300U << 17);
   mwVer.validation |= (NXP_EN_PN557 << 11);
   mwVer.android_version = NXP_ANDROID_VER;
   DLOG_IF(INFO, nfc_debug_enabled)
