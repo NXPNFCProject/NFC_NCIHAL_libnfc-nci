8257f3e52313a3fd0f0b55b3afff7a90db3d707e
nxf31698 <suryaprakash.konduru@nxp.com>
2023-04-28 13:07:58 +0530

    Corrected config NCI_RESET_TYPE 0x01 handling
    When NCI_RESET_TYPE=0x01, Core Reset cmd is still going with Keep config
    on Device Reboot.
    Reboot --> Core Reset with Keep Config - --> First nfc reset -->
    Core Reset with Reset Config --> subsequent NFC Reset  -->
    Core Reset with Keep Config.
    This observed behavior is corrected to behave as expected for
    NCI_RESET_TYPE with 0x01.
    
    Test: Set NCI_RESET_TYPE to 0x01 in libnfc-nci.conf and reboot and
    perform NFC service off & on
    Bug: 271804907

--- SN100x/src/nfc/nfc/nfc_task.cc
+++ SN100x/src/nfc/nfc/nfc_task.cc
@@ -407,22 +407,16 @@ uint32_t nfc_task(__attribute__((unused)) uint32_t arg) {
       nfc_set_state(NFC_STATE_CORE_INIT);
 
       /* Reset NCI configurations base on NAME_NCI_RESET_TYPE setting */
-      if (nfc_nci_reset_type == 0x02
-#if (NXP_EXTNS == TRUE)
-          || nfc_nci_reset_keep_cfg_enabled
-#endif
-      ) {
+      if (nfc_nci_reset_type == 0x02 || nfc_nci_reset_keep_cfg_enabled) {
         /* 0x02, keep configurations. */
         nci_snd_core_reset(NCI_RESET_TYPE_KEEP_CFG);
         nfc_nci_reset_keep_cfg_enabled = true;
       } else if (nfc_nci_reset_type == 0x01 &&
                  !nfc_nci_reset_keep_cfg_enabled) {
         /* 0x01, reset configurations only once every boot. */
-#if (NXP_EXTNS == TRUE)
+
         nci_snd_core_reset(NCI_RESET_TYPE_RESET_CFG);
-#else
-        nci_snd_core_reset(NCI_RESET_TYPE_KEEP_CFG);
-#endif
+
         nfc_nci_reset_keep_cfg_enabled = true;
       } else {
         /* Default, reset configurations every time*/
