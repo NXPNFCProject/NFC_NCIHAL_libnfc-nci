072dbdf8e3ff364d2ca8b1d93984db114c4732cc
nxf31698 <suryaprakash.konduru@nxp.com>
2023-05-22 19:38:34 +0530

    Remove obsolete fix from rw_i93_handle_error
    aosp/2022096 introduced an outdated patch that is no longer
    compatible with the current version.
    
    Bug: 281682898
    Test: R/W T5T

--- SN100x/src/nfc/tags/rw_i93.cc
+++ SN100x/src/nfc/tags/rw_i93.cc
@@ -3111,10 +3111,6 @@ void rw_i93_handle_error(tNFC_STATUS status) {
 
   if (rw_cb.p_cback) {
     rw_data.status = status;
-    if ((NFC_STATUS_TIMEOUT == rw_data.status) &&
-        (p_i93->sent_cmd != I93_CMD_STAY_QUIET)) {
-      p_i93->state = RW_I93_STATE_IDLE;
-    }
     switch (p_i93->state) {
       case RW_I93_STATE_IDLE: /* in case of RawFrame */
         event = RW_I93_INTF_ERROR_EVT;
