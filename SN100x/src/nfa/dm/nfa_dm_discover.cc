4ddef1b2381a71adb29ab43681c319e9eaca431c
nxf31698 <suryaprakash.konduru@nxp.com>
2023-08-07 14:23:24 +0530

    Update Copyright banner

--- SN100x/src/nfa/dm/nfa_dm_discover.cc
+++ SN100x/src/nfa/dm/nfa_dm_discover.cc
@@ -30,7 +30,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  Copyright 2018-2022 NXP
+ *  Copyright 2018-2023 NXP
  *
  ******************************************************************************/
 /******************************************************************************
