529cbdcb4060c7b599635c137bad1d6dd450a67c
nxf36763 <ashish.manohar@nxp.com>
2024-10-04 18:26:23 +0530

    Get SWP state support in Factory test
    Added support to process the get SWP state command for secure element
     and return the initialization status through NCI response.
    
    Change-Id: I4a9f03dd3b583bab01db523426c3984e7ca4ae6e

--- inc/NfcExtensionConstants.h
+++ inc/NfcExtensionConstants.h
@@ -125,6 +125,7 @@ constexpr uint8_t NCI_CORE_RESET_NTF_GID_VAL = 0x60;
 constexpr uint8_t NCI_CORE_RESET_NTF_OID_VAL = 0x00;
 constexpr uint8_t NCI_TEST_RF_SPC_NTF_OID_VAL = 0x3D;
 constexpr uint8_t DEAFULT_NXP_SYS_CLK_FREQ_SEL_VALUE = 2; /*CLK_FREQ_19_2MHZ*/
+constexpr uint8_t DEAFULT_NXP_SYS_CLK_SRC_SEL = 2;
 
 constexpr uint8_t DISABLE_LOG = 0x00;
 constexpr uint8_t ENABLE_LOG = 0x01;
