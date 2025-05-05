a78c7353a4a3450c36ed7be8b713eb3118147ac9
nxf45198 <fahim.akhtar@nxp.com>
2025-05-05 15:08:12 +0530

    Updated the copyright year and added copyright header
    Change-Id: I0f6113ebf2faea489ee77bde5afbc6020a2e6d8e

--- hardware/nxp/nfc/snxxx/libnfc_vendor_extn/src/core/nci/NciCommandBuilder.cpp
+++ hardware/nxp/nfc/snxxx/libnfc_vendor_extn/src/core/nci/NciCommandBuilder.cpp
@@ -1,6 +1,6 @@
 /**
  *
- *  Copyright 2024 NXP
+ *  Copyright 2024-2025 NXP
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
