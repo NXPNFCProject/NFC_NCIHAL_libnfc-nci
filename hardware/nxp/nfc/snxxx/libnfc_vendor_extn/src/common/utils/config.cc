76e6e7b87a896cb16aae1039c9bc165fbabbe977
nxf56319 <sai.shwethas@nxp.com>
2025-06-03 14:56:58 +0530

    Remove "ranges" header file dependency.
    Modify split API to getLine API.
    
    Change-Id: If03e5f2c494254a7b455d324634595b1d6158fb9

--- hardware/nxp/nfc/snxxx/libnfc_vendor_extn/src/common/utils/config.cc
+++ hardware/nxp/nfc/snxxx/libnfc_vendor_extn/src/common/utils/config.cc
@@ -36,7 +36,6 @@
 #include <android-base/logging.h>
 #include <android-base/parseint.h>
 #include <android-base/strings.h>
-#include <ranges>
 #include <phNxpLog.h>
 using namespace ::std;
 using namespace ::android::base;
@@ -44,7 +43,12 @@ using namespace ::android::base;
 namespace {
 
 bool parseBytesString(std::string in, std::vector<uint8_t>& out) {
-  vector<string> values = Split(in, ":");
+  std::stringstream ss(in);
+  string item;
+  vector<string> values;
+  while (std::getline(ss, item, ':')) {
+    values.push_back(item);
+  }
   if (values.size() == 0) return false;
   for (const string& value : values) {
     if (value.length() != 2) return false;
@@ -156,8 +160,9 @@ void ConfigFile::parseFromFile(const std::string& file_name) {
 }
 
 void ConfigFile::parseFromString(const std::string& config) {
-  for (auto lineR : std::views::split(config, '\n')) {
-    string line(lineR.begin(), lineR.end());
+  std::stringstream ss(config);
+  std::string line;
+  while (std::getline(ss, line)) {
     line = Trim(line);
     if (line.empty()) continue;
     if (line.at(0) == '#') continue;
