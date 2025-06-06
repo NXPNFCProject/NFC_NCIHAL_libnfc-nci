4d7ffa8cb0667de7577af5bf52e4ffe545d70510
nxf24178 <ravindra.yashvant.shinde@nxp.com>
2025-06-06 11:48:51 +0530

    Move proprietary logic to vendor extension library
    Change-Id: Ib415f6919f5a3a812c40e24f0cf97ea9dc39e349

--- hardware/nxp/nfc/snxxx/libnfc_vendor_extn/src/api/NfcExtensionApi.cpp
+++ hardware/nxp/nfc/snxxx/libnfc_vendor_extn/src/api/NfcExtensionApi.cpp
@@ -32,6 +32,7 @@
 #include "QTagHandler.h"
 #include "RfStateMonitor.h"
 #include "SrdHandler.h"
+#include "Type4Tag.h"
 #include "TransitConfigHandler.h"
 #include <phNxpLog.h>
 #include <stdint.h>
@@ -208,6 +209,7 @@ bool vendor_nfc_de_init() {
   NfcExtensionWriter::finalize();
   PlatformAbstractionLayer::finalize();
   ProprietaryExtn::finalize();
+  Type4Tag::finalize();
   resetNxpConfig();
   return true;
 }
@@ -254,6 +256,16 @@ NFCSTATUS vendor_nfc_handle_event(NfcExtEvent_t eventCode,
   case HANDLE_HAL_EVENT: {
     status = phNxpExtn_OnHandleHalEvent(eventData->hal_event,
                                         eventData->hal_event_status);
+    break;
+  }
+  case HANDLE_T4T_ENABLE: {
+    (void)eventData;
+    status = Type4Tag::getInstance()->enable();
+    break;
+  }
+  case HANDLE_T4T_UPDATE_PROP_PARAM: {
+    status = Type4Tag::getInstance()->updatePropParam(
+      eventData->nci_rsp_ntf.data_len, eventData->nci_rsp_ntf.p_data);
     break;
   }
   default: {
